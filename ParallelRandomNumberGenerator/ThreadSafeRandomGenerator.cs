d9a731b421760be0877afe33dd70e2426dc39039
Swati <Swati@DESKTOP-BUE85VU>
2016-10-11 11:26:02 +0530

    Add project files.

---
+++ ParallelRandomNumberGenerator/ThreadSafeRandomGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelRandomNumberGenerator
+{
+    public static class ThreadSafeRandomGenerator
+    {
+        private static int GetCryptoRandom()
+        {
+            using (var rng = new System.Security.Cryptography.RNGCryptoServiceProvider())
+            {
+                byte[] bytes = new byte[4];
+                rng.GetBytes(bytes);
+                return BitConverter.ToInt32(bytes, 0);
+            }
+        }
+
+        private static readonly ThreadLocal<Random> _local = new ThreadLocal<Random>(() => new Random(GetCryptoRandom()));
+ 
+        public static int GenerateWithInBounds(int minBound, int maxBound)
+        {
+           return _local.Value.Next(minBound, maxBound);
+        }
+    }
+}
