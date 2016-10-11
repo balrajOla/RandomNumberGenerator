e3142f78fe1b9743009fc05eaf7d38349555c7a1
balraj <erbalrajs@gmail.com>
2016-10-11 19:15:44 +0530

    Implemented core functionality

--- ParallelRandomNumberGenerator/ThreadSafeRandomGenerator.cs
+++ ParallelRandomNumberGenerator/ThreadSafeRandomGenerator.cs
@@ -8,8 +8,15 @@ using System.Threading.Tasks;
 
 namespace ParallelRandomNumberGenerator
 {
+    /// <summary>
+    /// Thread safe Class to generate Random Number
+    /// </summary>
     public static class ThreadSafeRandomGenerator
     {
+        /// <summary>
+        /// Method to generate GetCryptoRandom
+        /// </summary>
+        /// <returns></returns>
         private static int GetCryptoRandom()
         {
             using (var rng = new System.Security.Cryptography.RNGCryptoServiceProvider())
@@ -20,8 +27,17 @@ namespace ParallelRandomNumberGenerator
             }
         }
 
+        /// <summary>
+        /// Thread safe access to create instance of Random class
+        /// </summary>
         private static readonly ThreadLocal<Random> _local = new ThreadLocal<Random>(() => new Random(GetCryptoRandom()));
  
+        /// <summary>
+        /// Generate integer Random number
+        /// </summary>
+        /// <param name="minBound">Minimum bound to generate no.</param>
+        /// <param name="maxBound">Maximum bound to generate no.</param>
+        /// <returns>Random number generated</returns>
         public static int GenerateWithInBounds(int minBound, int maxBound)
         {
            return _local.Value.Next(minBound, maxBound);
